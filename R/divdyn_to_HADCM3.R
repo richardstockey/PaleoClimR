3aa33b9b32d55effb6e20d7351e2257d2c4ccfa4
richardstockey <50849895+richardstockey@users.noreply.github.com>
2024-02-01 14:25:35 +0000

    updates

--- R/divdyn_to_HADCM3.R
+++ R/divdyn_to_HADCM3.R
@@ -3,6 +3,7 @@
 # Rich Stockey 20240115
 # designed to convert DivDyn PBDB stages to HADCM3 stages used by Valdes et al. 2021
 ###################################################
+# 20240201 - updated to not be dependent on csv table on RGS macbook pro
 
 divdyn_to_HADCM3 <- function(divdyn_stage
     ){
@@ -17,7 +18,7 @@ divdyn_to_HADCM3 <- function(divdyn_stage
   stage_no_spaces <- divdyn_stage %>% str_replace(" ", "_")
 
   # at some point want to make this an RData file!
-  stage_translations <- read_csv("~/Phanero_niches/stage_translations.csv")
+  stage_translations <- load("stage_translations.RData")
   stage_translations <- filter(stage_translations, !is.na(stage_translations$diDyn_stage))
 
   Valdes_stage_age <- as.numeric()
