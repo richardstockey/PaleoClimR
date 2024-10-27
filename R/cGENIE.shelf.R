56fa07a788ac654d81e7d5f4fd388c5a2bdcf023
richardstockey <50849895+richardstockey@users.noreply.github.com>
2024-10-27 13:23:22 +0000

    Update cGENIE.shelf.R

--- R/cGENIE.shelf.R
+++ R/cGENIE.shelf.R
@@ -23,9 +23,6 @@ cGENIE.shelf <- function(nc.sum, shelf.depth = 3, array.only = TRUE, surf = FALS
   # If wanting to plot shelf, set array.only = FALSE to get spatial NetCDF variables back
   # -------------------------------------------------------------------------------------------------------
 
-  # Print a message to indicate the function is running
-  print("Function cGENIE.shelf is running")
-
   # Extract the variable array from the NetCDF summary data
   var <- nc.sum$var
 
@@ -43,20 +40,51 @@ cGENIE.shelf <- function(nc.sum, shelf.depth = 3, array.only = TRUE, surf = FALS
   # Determine the depth vector based on whether the surface layer is included
   depth.vec <- if (surf) 1:shelf.depth else 2:shelf.depth
 
-  # Loop through each longitude, latitude, and depth to populate the shelf array
-  for (lon in 1:length(nc.sum$lon)) {
-    for (lat in 1:length(nc.sum$lat)) {
-      for (depth in depth.vec) {
-        # Check if the current cell is on land
-        if (is.na(var[lon, lat, depth, time])) {
-          shelf.array[lon, lat, depth] <- NA
-        } else {
-          # Add your processing logic here
-        }
-      }
-    }
-  }
+  for(lon in 1:length(nc.sum$lon)){
+    for(lat in 1:length(nc.sum$lat)){
+      for(depth in depth.vec){
 
+        if(is.na(var[lon, lat, depth, time]) == TRUE){ #if we're on land
+          shelf.array[lon, lat, depth] <- NA
+          #print("we're on land")
+        } else{
+          # CHECK if statements here
+          if(lat == 36){
+            if(lon > 18){
+              var.N <- var[lon-18, lat, depth, time]
+            }
+            if(lon < 18){
+              var.N <- var[36+lon-18, lat, depth, time]
+            }
+
+          } else{ # if go over pole, swap to opposite longitude and stay at same latitude (check logic)
+            var.N <- var[lon, lat+1, depth, time]}
+          if(lat == 1){
+            if(lon > 18){
+              var.S <- var[lon-18, lat, depth, time]
+            }
+            if(lon < 18){
+              var.S <- var[36+lon-18, lat, depth, time]
+            }
+          } else{ # if go over pole, swap to opposite longitude and stay at same latitude (check logic)
+            var.S <- var[lon, lat-1, depth, time]}
+          if(lon == 36){
+            var.E <- var[1, lat, depth, time]} else{ # if going east from 36, go to 1
+              var.E <- var[lon+1, lat, depth, time]}
+          if(lon == 1){
+            var.W <- var[36, lat, depth, time]} else{ # if going east from 1, go to 36
+              var.W <- var[lon-1, lat, depth, time]}
+
+          if(is.na(var.N) == TRUE | is.na(var.S) == TRUE | is.na(var.E) == TRUE | is.na(var.W) == TRUE){
+            shelf.array[lon, lat, depth] <- var[lon, lat, depth, time]
+          } else{
+            shelf.array[lon, lat, depth] <- NA
+          }
+        }
+      }}}
+
+
+  return(shelf.array)
   # Return the shelf array
   return(shelf.array)
 }
